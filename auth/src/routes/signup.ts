e0f17b40da2322c0dbee04d0f3ddf3f1ab1dcffc
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-22 22:46:52 +0530

    added validations in auth

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,25 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post('/api/users/signup', (req, res) => {
+router.post('/api/users/signup', [
+    body('email')
+        .isEmail()
+        .withMessage('Email must be valid'),
+    body('password')
+        .trim()
+        .isLength({ min: 4, max: 20 })
+        .withMessage('Password must be between 4 to 20 characters')
+], (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        res.status(400).send(errors.array());
+    }
+
+    // const { email, password } = req.body;
+
     res.send('CKMB from signup!');
 });
 
