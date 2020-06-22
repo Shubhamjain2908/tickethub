57957e817242ae35c7d0e93f984f71758db357de
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-23 01:59:21 +0530

    connecting to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -19,10 +20,26 @@ app.use(signupRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
-})
+});
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000!')
-});
+const start = async () => {
+    try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to MongoDB!!!')
+    } catch (err) {
+        console.error('Mongoose error => ', err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!')
+    });
+
+};
+
+start();
