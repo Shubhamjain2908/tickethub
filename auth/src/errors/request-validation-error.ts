ccb03e7beffab22a6c9e1589fad62585b9251492
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-23 01:28:07 +0530

    creating custom error class

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,10 +1,19 @@
-import { ValidationError } from "express-validator";
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
 
-export class RequestValidationError extends Error {
+export class RequestValidationError extends CustomError {
+    statusCode = 400;
+
     constructor(public errors: Array<ValidationError>) {
-        super();
+        super('Invalid request parameters');
 
         // Only because we are extending a build in class
         Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+
+    serializeErrors() {
+        return this.errors.map(error => {
+            return { message: error.msg, field: error.param };
+        });
     }
 }
