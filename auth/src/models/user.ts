3f66f0bb5f280836f8969f1ee7eb6c2ca045431b
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-23 23:14:59 +0530

    defining document & mode defination for user model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -6,6 +6,17 @@ interface UserAttrs {
     password: string;
 }
 
+// An interface that describes the properties that a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties that a User Document has
+interface UserDoc extends mongoose.Document {
+    email: string;
+    password: string;
+}
+
 const userSchema = new mongoose.Schema({
     email: {
         type: String,
@@ -16,11 +27,10 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
-
-const User = mongoose.model('User', userSchema);
-
-const buildUser = (attrs: UserAttrs) => {
+userSchema.statics.build = (attrs: UserAttrs) => {
     return new User(attrs);
 };
 
-export { User, buildUser };
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
