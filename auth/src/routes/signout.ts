0d21443a12700c4bac6f4437c530145ce88fb23e
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-25 01:17:58 +0530

    signin & signout handlers

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,11 @@
-import express from "express";
+import express from 'express';
 
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-    res.send('CKMB from signout!');
+  req.session = null;
+
+  res.send({});
 });
 
 export { router as signoutRouter };
