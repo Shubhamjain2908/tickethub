4415724d22697e1d4a1f29d3cc8b88698378bba3
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-26 03:27:12 +0530

    created a reusable buildClient

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,31 +1,14 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-    console.log('CUrrent user => ', currentUser)
-    return <h1>Landing page</h1>;
+    return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
 
 // Function called during ssr.. takes the props from this func & push it into the component 
-LandingPage.getInitialProps = async () => {
-
-    if (typeof window === 'undefined') {
-        // We are on the server!
-        // http://NAME_OF_SERVICE_IN_WORKSPACE.NAMESPACE.svc.cluster.local
-        // req should be made to http://ingress-nginx-controller.ingress-nginx.svc.cluster.local
-        const { data } = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-            headers: {
-                Host: 'tickethub.dev'
-            }
-        }
-        );
-        return { data };
-    } else {
-        // We are on the browser!
-        // req should be made with base url ''
-        const { data } = await axios.get('/api/users/currentuser');
-        return data;
-    }
-}
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context);
+    const { data } = await client.get('/api/users/currentuser');
+    return data;
+};
 
 export default LandingPage;
