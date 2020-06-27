9d243bdb000ab7c150b03eeafcfcf27da0f74609
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-28 02:18:07 +0530

    initialize tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,25 @@
+import { errorHandler, NotFoundError } from '@tickethub/common';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import express from 'express';
+import 'express-async-errors';
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: process.env.NODE_ENV !== 'test'    // Must be on https connection except on test env
+    })
+);
+
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
+
---
+++ tickets/src/index.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import { app } from './app';
+
+const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
+    try {
+        await mongoose.connect('mongodb://tickets-mongo-srv:27017/auth', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log('Connected to MongoDB!!!')
+    } catch (err) {
+        console.error('Mongoose error => ', err);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening on port 3000!')
+    });
+
+};
+
+start();
