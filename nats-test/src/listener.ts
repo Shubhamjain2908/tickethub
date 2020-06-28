1ad7504663b1acd2d43beefd8985d2ebb9e8ed08
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-29 01:09:51 +0530

    adding queue group: to limit events

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,16 +1,17 @@
+import { randomBytes } from 'crypto';
 import nats, { Message } from 'node-nats-streaming';
 
 console.clear();
 
 // Connecting using port-forwarding: kubectl port-forward POD_NAME PORT_ON_LOCAL:PORT_ON_POD
-const stan = nats.connect('tickethub', '123', {
+const stan = nats.connect('tickethub', randomBytes(4).toString('hex'), {
     url: 'http://localhost:4222'
 });
 
 stan.on('connect', () => {
     console.log('Listener connected to NATS');
 
-    const subscription = stan.subscribe('ticket:created');
+    const subscription = stan.subscribe('ticket:created', 'order-service-queue-group');
 
     subscription.on('message', (msg: Message) => {
         const data = msg.getData();
