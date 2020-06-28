16134fd1474d9345330a1f9a4bdb46dee8122f6a
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-28 21:14:50 +0530

    update controller completed with CRUD

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,27 +1,44 @@
-// import { body } from 'express-validator';
 import {
     NotAuthorizedError,
     NotFoundError,
-    requireAuth
+    requireAuth,
+    validateRequest
 } from '@tickethub/common';
 import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req: Request, res: Response) => {
-    const ticket = await Ticket.findById(req.params.id);
+router.put(
+    '/api/tickets/:id',
+    requireAuth,
+    [
+        body('title').not().isEmpty().withMessage('Title is required'),
+        body('price')
+            .isFloat({ gt: 0 })
+            .withMessage('Price must be greater than 0'),
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const ticket = await Ticket.findById(req.params.id);
 
-    if (!ticket) {
-        throw new NotFoundError();
-    }
+        if (!ticket) {
+            throw new NotFoundError();
+        }
 
-    if (ticket.userId !== req.currentUser!.id) {
-        throw new NotAuthorizedError();
-    }
+        if (ticket.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
 
-    res.send(ticket);
-});
+        ticket.set({
+            title: req.body.title,
+            price: req.body.price
+        });
+        await ticket.save();
+
+        res.send(ticket);
+    });
 
 export { router as updateTicketRouter };
 
