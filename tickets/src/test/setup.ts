4fb0e3d11e145f7cca8f346c2c133df8a48f96e3
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-28 18:27:10 +0530

    faking authentication during test

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,12 +1,11 @@
+import jwt from 'jsonwebtoken';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import request from 'supertest';
-import { app } from '../app';
 
 declare global {
     namespace NodeJS {
         interface Global {
-            signin(): Promise<Array<string>>
+            signin(): Array<string>
         }
     }
 }
@@ -38,18 +37,25 @@ afterAll(async () => {
     await mongoose.connection.close();
 });
 
-global.signin = async () => {
-    const email = 'test@test.com';
-    const password = 'password';
+global.signin = () => {
+    // Build a JWT payload. { id, email }
+    const payload = {
+        id: '00000000000',
+        email: 'test@test.com'
+    }
 
-    const response = await request(app)
-        .post('/api/users/signup')
-        .send({
-            email, password
-        })
-        .expect(201);
+    // Create the JWT!
+    const token = jwt.sign(payload, process.env.JWT_KEY!)
 
-    const cookie = response.get('Set-Cookie');
+    // Build session Object, { jwt: MY_JWT }
+    const session = { jwt: token }
 
-    return cookie;
+    // Turn that session into JSON
+    const sessionJSON = JSON.stringify(session);
+
+    // Take JSON & encode it as base64
+    const base64 = Buffer.from(sessionJSON).toString('base64');
+
+    // returns a string thats the cookie with encoded data (express=sess)
+    return [`express:sess=${base64}`];
 }
