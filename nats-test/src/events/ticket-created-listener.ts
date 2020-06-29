fa4541e2213863bb26cc8b33812664f96773a97a
Shubham Jain <shubhamjain2908@gmail.com>
2020-06-30 02:29:55 +0530

    added Generics & enums to strictly typed the events

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,14 @@
 import { Message } from 'node-nats-streaming';
 import { Listener } from './base-listener';
+import { Subjects } from './subjects';
+import { TicketCreatedEvent } from './ticket-created-event';
 
-export class TicketCreatedListener extends Listener {
-    subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    // defining a type so that we can't change that in future
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
     queueGroupName = 'payments-service';
 
-    onMessage(data: any, msg: Message): void {
+    onMessage(data: TicketCreatedEvent['data'], msg: Message): void {
         console.log('Event data!', data);
 
         msg.ack();
