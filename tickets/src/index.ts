6b1c2fe4fc7decb0a19a160964b42c82d5564e4f
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-01 01:26:31 +0530

    closing the nats connection

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -11,6 +11,16 @@ const start = async () => {
     }
     try {
         await natsWrapper.connect('tickethub', 'asdasdasdasd', 'http://nats-srv:4222');
+
+        natsWrapper.client.on('connect', () => {
+            console.log('Nats connection closed!');
+            process.exit();
+        });
+
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
