c82e0b01d2aaae606943f59b11dad270a3bbb827
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-04 02:41:21 +0530

    basic routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,10 +3,10 @@ import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import express from 'express';
 import 'express-async-errors';
-// import { indexTicketRouter } from './routes';
-// import { createTicketRouter } from './routes/new';
-// import { showTicketRouter } from './routes/show';
-// import { updateTicketRouter } from './routes/update';
+import { indexOrderRouter } from './routes';
+import { deleteOrderRouter } from './routes/delete';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
 app.set('trust proxy', true);
@@ -19,10 +19,10 @@ app.use(
 );
 app.use(currentUser);
 
-// app.use(indexTicketRouter);
-// app.use(createTicketRouter);
-// app.use(showTicketRouter);
-// app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
