fdd6ffb089b121cc45ad9772859857b3bb37c6e7
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-05 01:52:59 +0530

    optimistic concurrency control

--- tickets/src/models/ticket.ts
+++ tickets/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
     title: string;
@@ -14,6 +15,7 @@ interface TicketDoc extends mongoose.Document {
     title: string;
     price: number;
     userId: string;
+    version: number;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -36,12 +38,12 @@ const ticketSchema = new mongoose.Schema(
             transform(doc, ret) {
                 ret.id = ret._id;
                 ret._id = undefined;
-            },
-            versionKey: false
+            }
         }
     }
 );
-
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
     return new Ticket(attrs);
@@ -50,3 +52,4 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
 export { Ticket };
+
