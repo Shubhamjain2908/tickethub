f4dc339aaa0d15607a56aeeb28e81f2590524925
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-04 19:40:49 +0530

    ticket-created listener

---
+++ orders/src/events/listeners/ticket-created-listeners.ts
@@ -0,0 +1,19 @@
+import { Listener, Subjects, TicketCreatedEvent } from '@tickethub/common';
+import { Message } from 'node-nats-streaming';
+import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+        const { title, price } = data;
+        const ticket = Ticket.build({
+            title, price
+        });
+        await ticket.save();
+
+        msg.ack();
+    }
+}
