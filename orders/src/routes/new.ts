5d6b3db6f3a994604a7633f11c334b7f31bee4b5
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-04 14:44:26 +0530

    finding reserved tickets

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,9 @@
-import { requireAuth, validateRequest } from '@tickethub/common';
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@tickethub/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Order } from '../models/order';
+import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
@@ -16,6 +18,37 @@ router.post('/api/orders',
     ],
     validateRequest,
     async (req: Request, res: Response) => {
+        const { ticketId } = req.body;
+
+        // Find the ticket the user is trying to order in the database
+        const ticket = await Ticket.findById(ticketId)
+        if (!ticket) {
+            throw new NotFoundError();
+        }
+
+        // Make sure that the ticket is not served
+        // Run query to look all orders. FInd an order where the ticket 
+        // is the ticket we just found *and* the orders status is *not* cancelled.
+        // If we find an order from that menas the ticket *is* reserved
+        const existingOrder = await Order.findOne({
+            ticket: ticket,
+            status: {
+                $in: [
+                    OrderStatus.Created,
+                    OrderStatus.AwaitingPayment,
+                    OrderStatus.Complete
+                ]
+            }
+        });
+        if (existingOrder) {
+            throw new BadRequestError('Ticket is already reserved');
+        }
+
+        // Caclulate an expiration date of this order
+
+        // Build the order & save it to the database
+
+        // Publish an evemt saying that an order was created
         res.send({});
     }
 );
