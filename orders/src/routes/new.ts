f7338bdb3c3e620b357b08a7fe18f457b49b58b9
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-04 15:56:58 +0530

    setting expiration & building the order

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,13 @@
-import { BadRequestError, NotFoundError, requireAuth, validateRequest } from '@tickethub/common';
+import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateRequest } from '@tickethub/common';
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Order } from '../models/order';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post('/api/orders',
     requireAuth,
@@ -32,11 +35,21 @@ router.post('/api/orders',
         }
 
         // Caclulate an expiration date of this order
+        const expiration = new Date();
+        expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
 
         // Build the order & save it to the database
+        const order = Order.build({
+            userId: req.currentUser!.id,
+            status: OrderStatus.Created,
+            expiresAt: expiration,
+            ticket
+        });
+        await order.save();
 
         // Publish an evemt saying that an order was created
-        res.send({});
+
+        res.status(201).send(order);
     }
 );
 
