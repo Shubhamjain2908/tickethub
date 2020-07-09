f89cb86fa19b9a75971ea87b5145ff3dff6800f8
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-10 03:12:50 +0530

    order created listener & test for it

---
+++ payments/src/events/listeners/__test__/order-created-listener.test.ts
@@ -0,0 +1,48 @@
+import { OrderCreatedEvent, OrderStatus } from '@tickethub/common';
+import mongoose from 'mongoose';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../../models/order';
+import { natsWrapper } from '../../../nats-wrapper';
+import { OrderCreatedListener } from '../order-created-listener';
+
+const setup = async () => {
+    const listener = new OrderCreatedListener(natsWrapper.client);
+
+    const data: OrderCreatedEvent['data'] = {
+        id: mongoose.Types.ObjectId().toHexString(),
+        version: 0,
+        expiresAt: new Date().toISOString(),
+        status: OrderStatus.Created,
+        ticket: {
+            id: 'CKMB',
+            price: 10
+        },
+        userId: 'lsksl'
+    };
+
+    // @ts-ignore
+    const msg: Message = {
+        ack: jest.fn()
+    };
+
+    return { listener, data, msg };
+};
+
+it('replicates the order info', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    const order = await Order.findById(data.id);
+
+    expect(order).toBeDefined();
+    expect(order!.price).toEqual(data.ticket.price);
+});
+
+it('acks the message', async () => {
+    const { listener, data, msg } = await setup();
+
+    await listener.onMessage(data, msg);
+
+    expect(msg.ack).toHaveBeenCalled();
+});
--- payments/src/models/order.ts
+++ payments/src/models/order.ts
@@ -64,3 +64,5 @@ orderSchema.statics.build = (attrs: OrderAttrs) => {
 };
 
 const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
