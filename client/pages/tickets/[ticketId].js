008ae140327340d973605b3630beacf3343443c0
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-15 00:54:52 +0530

    navigating to wildcard route

---
+++ client/pages/tickets/[ticketId].js
@@ -0,0 +1,17 @@
+const TicketShow = ({ ticket }) => {
+    return (
+        <div>
+            <h1>{ticket.title}</h1>
+            <h4>Price: {ticket.price}</h4>
+        </div>
+    );
+};
+
+TicketShow.getInitialPops = async (context, client) => {
+    const { ticketId } = context.query;
+    const { data } = await client.get(`/api/tickets/${ticketId}`);
+
+    return { ticket: data };
+};
+
+export default TicketShow;
