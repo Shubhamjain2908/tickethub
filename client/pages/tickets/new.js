c3df3dddad3d50520e785604744ed114fcbe8e8a
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-15 00:42:39 +0530

    listing all tickets

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,3 +1,4 @@
+import Router from 'next/router';
 import { useState } from 'react';
 import useRequest from '../../hooks/use-request';
 
@@ -10,7 +11,7 @@ const NewTicket = () => {
         body: {
             title, price
         },
-        onSuccess: (ticket) => console.log(ticket)
+        onSuccess: () => Router.push('/')
     });
 
     const onSubmit = async (e) => {
